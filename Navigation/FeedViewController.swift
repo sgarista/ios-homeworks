660121fe9c2af8b2cbb4372c31209cd120ae827f
Igor Litvinov <ig.litvinov@gmail.com>
2023-06-05 23:04:25 +0300

    formatted the code

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -21,8 +21,8 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-                view.backgroundColor = .systemGray2
-                title = "Feed"
+        view.backgroundColor = .systemGray2
+        title = "Feed"
 
         view.addSubview(postButton)
 
