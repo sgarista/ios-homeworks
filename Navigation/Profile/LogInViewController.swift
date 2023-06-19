6121829afc36c7bb82c6dd9fcc5dcc7d698703f1
Igor Litvinov <ig.litvinov@gmail.com>
2023-06-19 19:06:30 +0300

    improved keyboard

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -4,17 +4,40 @@ import UIKit
 class LogInViewController: UIViewController {
 
 
-    var loginImageView: UIImageView = {
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.backgroundColor = .systemBackground
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+
+        return scrollView
+    }()
+
+    private lazy var contentView: UIView = {
+
+        let contentView = UIView()
+        contentView.backgroundColor = .white
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+
+        return contentView
+    }()
+
+
+    private lazy var loginImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "Logo")
         imageView.translatesAutoresizingMaskIntoConstraints = false
+
         return imageView
     }()
 
-    var loginTextField: UITextField = {
+    private lazy var loginTextField: UITextField = {
         let login = UITextField()
 
         login.translatesAutoresizingMaskIntoConstraints = false
+
         login.textColor = .black
         login.font = .systemFont(ofSize: 16, weight: .regular)
         login.tintColor = .lightGray
@@ -27,15 +50,18 @@ class LogInViewController: UIViewController {
         login.backgroundColor = .systemGray6
         login.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: login.frame.height))
         login.leftViewMode = .always
-        login.keyboardType = UIKeyboardType.default
-        login.returnKeyType = UIReturnKeyType.done
-        login.clearButtonMode = UITextField.ViewMode.whileEditing
-        
+        login.autocorrectionType = .no
+        login.keyboardType = .default
+        login.returnKeyType = .done
+        login.clearButtonMode = .whileEditing
+        login.contentVerticalAlignment = .center
+
+        login.delegate = self
 
         return login
     }()
 
-    var passwordTextField: UITextField = {
+    private lazy var passwordTextField: UITextField = {
         let password = UITextField()
 
         password.translatesAutoresizingMaskIntoConstraints = false
@@ -52,11 +78,17 @@ class LogInViewController: UIViewController {
         password.backgroundColor = .systemGray6
         password.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: password.frame.height))
         password.leftViewMode = .always
+        password.keyboardType = .default
+        password.returnKeyType = .done
+        password.clearButtonMode = .whileEditing
+        password.contentVerticalAlignment = .center
+
+        password.delegate = self
 
         return password
     }()
 
-    var stackView: UIStackView = {
+    private lazy var stackView: UIStackView = {
         var stackView = UIStackView()
 
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -76,13 +108,13 @@ class LogInViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Log In", for: .normal)
         button.layer.cornerRadius = 10
-        let bluePixel = UIImage(named: "BluePixel")
-        button.setBackgroundImage(bluePixel, for: .normal)
-        button.setBackgroundImage(bluePixel?.alpha(0.8), for: .selected)
-        button.setBackgroundImage(bluePixel?.alpha(0.8), for: .highlighted)
-        button.setBackgroundImage(bluePixel?.alpha(0.8), for: .disabled)
+        button.setBackgroundImage(UIImage(named: "BluePixel"), for: .normal)
+        button.setBackgroundImage(UIImage(named: "BluePixel")?.alpha(0.8), for: .selected)
+        button.setBackgroundImage(UIImage(named: "BluePixel")?.alpha(0.8), for: .highlighted)
+        button.setBackgroundImage(UIImage(named: "BluePixel")?.alpha(0.8), for: .disabled)
         button.setTitleColor(.white, for: .normal)
         button.clipsToBounds = false
+        button.layer.masksToBounds = true
 
         return button
     }()
@@ -97,26 +129,49 @@ class LogInViewController: UIViewController {
         loginStatusButton.addTarget(UIEvent(), action: #selector(buttonPressed(_:)), for: .touchUpInside)
 
 
+        // При использовании кода из лекции, каждое нажатие на текстфилд, поднимало экран все выше и выше.  Поэтому я использовал код ниже.
+
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
     }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+
+        let keyboardHeight = keyboardFrame.height
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        scrollView.contentInset = UIEdgeInsets.zero
+    }
+
 
     private func setupView() {
 
         view.backgroundColor = .white
         navigationController?.navigationBar.isHidden = true
-        navigationController?.navigationBar.prefersLargeTitles = false
 
     }
 
 
     private func addSubviews() {
 
-        view.addSubview(loginImageView)
+        view.addSubview(scrollView)
+
+        scrollView.addSubview(contentView)
+        contentView.addSubview(loginImageView)
 
         stackView.addArrangedSubview(loginTextField)
         stackView.addArrangedSubview(passwordTextField)
-        view.addSubview(stackView)
 
-        view.addSubview(loginStatusButton)
+        contentView.addSubview(stackView)
+        contentView.addSubview(loginStatusButton)
 
     }
 
@@ -125,22 +180,33 @@ class LogInViewController: UIViewController {
         let safeAreaLayoutGuide = view.safeAreaLayoutGuide
 
         NSLayoutConstraint.activate([
-            loginImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 120),
-            loginImageView.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+
+            scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            contentView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+
+            loginImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 120),
+            loginImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             loginImageView.heightAnchor.constraint(equalToConstant: 100),
             loginImageView.widthAnchor.constraint(equalToConstant: 100),
 
             stackView.topAnchor.constraint(equalTo: loginImageView.bottomAnchor, constant: 120),
-            stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor,constant: -16),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,constant: -16),
             stackView.heightAnchor.constraint(equalToConstant: 100),
-            
 
             loginStatusButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 16),
-            loginStatusButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            loginStatusButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            loginStatusButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            loginStatusButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             loginStatusButton.heightAnchor.constraint(equalToConstant: 50)
-
         ])
     }
 
@@ -150,10 +216,7 @@ class LogInViewController: UIViewController {
         navigationController?.pushViewController(ProfileViewController(), animated: true)
 
     }
-
 }
-
-
 
 
 
@@ -165,5 +228,14 @@ extension UIImage {
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return newImage
+    }
+}
+
+extension LogInViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+
+        return true
     }
 }
